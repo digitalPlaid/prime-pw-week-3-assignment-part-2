6b65540dab03fde5def9c97c0d943b1b11b8ce4e
Dan St. Aubin <staubindj@gmail.com>
2021-06-24 14:55:08 -0500

    finished

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -49,9 +49,22 @@ console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
-
+idx = 0;
+while (idx<stars.length) {
+  console.log(stars[idx ]);
+  idx++;
+}
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
-
+i = 0;
+while (i < 6) {
+  console.log(i);
+  i++;
+}
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+i = 10;
+while (i > 4) {
+  console.log(i)
+  i--;
+}
