4e4a9c42272edb5228eebc95be40d2e61864facb
Dan St. Aubin <staubindj@gmail.com>
2021-06-24 14:10:57 -0500

    thru 1.a

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -13,7 +13,10 @@ for (let i=0; i<4; i++) {
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
-
+// I am assuming like in the example that from a to b is inclusive-disclusive. I.e. stop one short of the second parameter.
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
